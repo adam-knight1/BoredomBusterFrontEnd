1d64ba17f80da992db0e84aeb7cfa1e299f6539f
adam-knight1 <adam.knight@mykenzie.snhu.edu>
2024-03-11 23:53:49 -0400

    Added link import statement for back button in TriviaGame.tsx

--- components/TriviaGame.tsx
+++ components/TriviaGame.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import Link from 'next/link';
 
 const categories = ["music", "mathematics", "geography", "scienceNature", "general", "entertainment", "toysGames", "peoplePlaces"];
 
