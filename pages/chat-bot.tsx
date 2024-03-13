0c8337cae6cbf39c5f77a9f1b1d7a40bd04c531b
adam-knight1 <adam.knight@mykenzie.snhu.edu>
2024-03-13 15:52:03 -0400

    Added different greeting for AI page

--- pages/chat-bot.tsx
+++ pages/chat-bot.tsx
@@ -5,8 +5,8 @@ import Link from 'next/link';
 const ChatBotPage: React.FC = () => {
   return (
     <main className="flex flex-col items-center justify-center min-h-screen bg-secondary p-4 text-gray-800">
-      <h1 className="text-5xl font-bold mb-6">Hello! My Name is Banjo!</h1>
-      <p className="text-xl font-medium mb-4">Your Personal AI Assistant.  Ask Me Anything!</p>
+      <h1 className="text-5xl font-bold mb-6">Hello! My Name is Banjo, Your AI Assistant!</h1>
+      <p className="text-xl font-medium mb-4">How Can I Help You Today? </p>
       <ChatBot />
       <Link href="/" className="mt-10 btn bg-accent hover:bg-red-700 text-white font-semibold rounded-full py-2 px-6">Back to Home</Link>
     </main>
