153ca4f49f0448b70f70380574df1b7a0154e8a2
adam-knight1 <adam.knight@mykenzie.snhu.edu>
2024-03-19 00:44:04 -0400

    interfacing with chess.js framework for move validation, implementing chessGame.tsx

--- components/ChessGame.tsx
+++ components/ChessGame.tsx
@@ -1,50 +1,24 @@
 import React, { useState } from 'react';
 import dynamic from 'next/dynamic';
-//import Chessboard from 'chessboardjsx';
+import { Chess } from 'chess.js'
 
 const Chessboard = dynamic(() => import('chessboardjsx'), { ssr: false });
 
-
 const ChessGame: React.FC = () => {
+  const [game, setGame] = useState<ChessInstance>(new Chess());
   const [position, setPosition] = useState('start');
-  const [fen, setFen] = useState('start');
 
-  const handleMove = async (sourceSquare: string, targetSquare: string) => {
-    const move = `${sourceSquare}-${targetSquare}`;
+  const handleMove = async (sourceSquare, targetSquare) => {
+    // Make the move on the front end first
+    const move = game.move({ from: sourceSquare, to: targetSquare, promotion: 'q' });
 
-    // Construct the API endpoint
-    const backendBaseUrl = process.env.NEXT_PUBLIC_API_BASE_URL || 'http://localhost:8080';
-    const apiUrl = `${backendBaseUrl}/api/chess/move`;
+      console.log(`Attempting move from ${sourceSquare} to ${targetSquare}`);
 
-    // Send the move to the backend and wait for the response
-    try {
-      const response = await fetch(apiUrl, {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-        body: `playerMove=${move}`
-      });
-
-      if (!response.ok) {
-        throw new Error('Network response was not ok');
+      // Ensure the source and target squares are different
+      if (sourceSquare === targetSquare) {
+        console.error("Invalid move: source and target squares are the same.");
+        return;
       }
 
-      // Assuming the backend returns the new FEN string after the move
-      const newFen = await response.text();
-      setFen(newFen); // Update the local FEN state
-    } catch (error) {
-      console.error('There was a problem with the fetch operation:', error);
-    }
-  };
 
-  return (
-    <Chessboard
-      width={320}
-      position={fen}
-      onDrop={({ sourceSquare, targetSquare }) => {
-        handleMove(sourceSquare, targetSquare);
-      }}
-    />
-  );
-};
 
-export default ChessGame;
