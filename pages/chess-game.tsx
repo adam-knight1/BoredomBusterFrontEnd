780fefbf03da8e8872b315bdbd3afa2165a17224
adam-knight1 <adam.knight@mykenzie.snhu.edu>
2024-03-19 02:44:45 -0400

    changed welcome message on chess page

--- pages/chess-game.tsx
+++ pages/chess-game.tsx
@@ -4,7 +4,7 @@ import ChessGame from '../components/ChessGame';
 const ChessGamePage: React.FC = () => {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-secondary p-24 text-primary">
-     <h1 className="text-5xl font-bold mb-6">Let's Play Chess</h1>
+     <h1 className="text-5xl font-bold mb-6">Here You Can Play Chess</h1>
      <p className="text-lg text-gray-500 mb-8"> Good Luck </p>
      <ChessGame />
     </main>
