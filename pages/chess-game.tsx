e3b5f4eb40be790fb0ee73a0e8e4c03b5a064a29
adam-knight1 <adam.knight@mykenzie.snhu.edu>
2024-03-19 02:17:26 -0400

    updated syntax in chess-game

--- pages/chess-game.tsx
+++ pages/chess-game.tsx
@@ -5,7 +5,6 @@ const ChessGamePage: React.FC = () => {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-secondary p-24 text-primary">
       <h1 className="text-5xl font-bold mb-6">Let's Play Chess</h1>
-        <p className="text-lg text-gray-500 mb-8"> Good Luck </p>
       <div className="mt-10">
         <ChessGame />
       </div>
